ff251d0ce29bb6a4102c4eed76a211fcfa6ad234
Teo Paolucci <teo.paolucci@gmail.com>
2015-12-03 11:41:14 -0500

    Timestamps added to Post model

--- models/Post.js
+++ models/Post.js
@@ -3,18 +3,23 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var postSchema = new Schema({
-  userID: {
-    type: String,
-    required: true
+var postSchema = new Schema(
+  {
+    userID: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    title: String,
+    body: String
+    //TODO: add img url and tags
   },
-  username: {
-    type: String,
-    required: true
-  },
-  title: String,
-  body: String
-  //TODO: add img url and tags
-})
+  {
+    timestamps: true
+  }
+);
 
 module.exports = postSchema;
